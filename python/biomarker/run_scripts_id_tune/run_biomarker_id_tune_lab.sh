64f8305be8cfffad005b99d5f7e663a2908babb7
jiaangyao <jiaang_yao@berkeley.edu>
2023-06-25 05:15:04 -0700

    Changed permission for run script

--- python/biomarker/run_scripts_id_tune/run_biomarker_id_tune_lab.sh
+++ python/biomarker/run_scripts_id_tune/run_biomarker_id_tune_lab.sh
@@ -1,4 +1,4 @@
 # conda init bash
 conda activate envTorch201
 
-HYDRA_FULL_ERROR=1 python ../training/biomarker_id_tune.py --config-name config_tune parallel=lab_ray_cpu tune.num_samples=200
+HYDRA_FULL_ERROR=1 python ../training/biomarker_id_tune.py --config-name config_tune parallel=lab_ray_cpu tune.num_samples=240
