76a4dfcf7b10db15e0918a4cb44ba2e2161934a7
Jiaang Yao <jiaang_yao@berkeley.edu>
2023-06-26 14:46:50 -0700

    Further updated configs with right logging and data path and run script

--- python/biomarker/run_scripts_id/run_biomarker_id_sweep_ur_lab.sh
+++ python/biomarker/run_scripts_id/run_biomarker_id_sweep_ur_lab.sh
@@ -5,3 +5,8 @@ conda activate envTorch201
 HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=lab_ray_cpu_batch patient=RCS02R preproc=ur3s
 HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=lab_ray_cpu_batch patient=RCS08R preproc=ur3s
 HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=lab_ray_cpu_batch patient=RCS11L preproc=ur3s
+
+# ur of 4_5s
+HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=lab_ray_cpu_batch patient=RCS02R preproc=ur4_5s
+HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=lab_ray_cpu_batch patient=RCS08R preproc=ur4_5s
+HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=lab_ray_cpu_batch patient=RCS11L preproc=ur4_5s
