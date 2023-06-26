c576474e19b527ec384b6c69f860d49d79f01073
Jiaang Yao <jiaang_yao@berkeley.edu>
2023-06-26 08:31:42 -0700

    Modified final run scripts

--- python/biomarker/run_scripts_id/run_biomarker_id_sweep_lambda.sh
+++ python/biomarker/run_scripts_id/run_biomarker_id_sweep_lambda.sh
@@ -1,10 +1,8 @@
 # conda init bash
 conda activate envTorch201
 
-# HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_model parallel=lambda_ray_cpu_batch patient=RCS02R
-# HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_model parallel=lambda_ray_cpu_batch patient=RCS08R
-HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_model parallel=lambda_ray_cpu_batch patient=RCS02R meta=med_RF_auc_default+med_MLP_auc_default+med_RNN_auc_default
-HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_model parallel=lambda_ray_cpu_batch patient=RCS08R meta=med_RF_auc_default+med_MLP_auc_default+med_RNN_auc_default
+HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_model parallel=lambda_ray_cpu_batch patient=RCS02R
+HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_model parallel=lambda_ray_cpu_batch patient=RCS08R
 HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_model parallel=lambda_ray_cpu_batch patient=RCS11L
 HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_model parallel=lambda_ray_cpu_batch patient=RCS12L
 HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_model parallel=lambda_ray_cpu_batch patient=RCS18L
