c9bc426d23d9a5b1cb0426c3844bf89f73788fbe
jiaangyao <jiaang_yao@berkeley.edu>
2023-06-26 10:33:02 -0400

    Modified run script for lambda and home

--- python/biomarker/run_scripts_dynamics/run_dynamics_id_sweep.sh
+++ python/biomarker/run_scripts_dynamics/run_dynamics_id_sweep.sh
@@ -1,4 +1,6 @@
 # conda init bash
 conda activate envTorch201
 
-HYDRA_FULL_ERROR=1 python ../training/biomarker_id_dynamics.py --multirun --config-name config_dynamics_sweep_model
+# HYDRA_FULL_ERROR=1 python ../training/biomarker_id_dynamics.py --multirun --config-name config_dynamics_sweep_model
+HYDRA_FULL_ERROR=1 python ../training/biomarker_id_dynamics.py --multirun --config-name config_dynamics_sweep_model parallel=home_ray_cpu_batch patient=RCS12L
+HYDRA_FULL_ERROR=1 python ../training/biomarker_id_dynamics.py --multirun --config-name config_dynamics_sweep_model parallel=home_ray_cpu_batch patient=RCS18L
