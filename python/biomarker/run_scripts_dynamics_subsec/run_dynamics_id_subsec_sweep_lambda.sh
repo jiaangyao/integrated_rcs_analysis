0c5782c8db6a31483a90e072b465f03e5d5c566f
jiaangyao <jiaang_yao@berkeley.edu>
2023-06-27 09:16:09 -0400

    Added run scripts for subsec dynamics

---
+++ python/biomarker/run_scripts_dynamics_subsec/run_dynamics_id_subsec_sweep_lambda.sh
@@ -0,0 +1,5 @@
+# conda init bash
+conda activate envTorch201
+
+# subsec dynamics sweep through model for RCS02R
+HYDRA_FULL_ERROR=1 python ../training/biomarker_id_dynamics.py --multirun --config-name config_dynamics_subsec patient=RCS02R parallel=lambda_ray_cpu_batch
