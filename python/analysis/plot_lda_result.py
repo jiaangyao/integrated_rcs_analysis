6168b4e94e1763a8af13103bd1ee39651b51ee90
Jiaang Yao <jiaang_yao@berkeley.edu>
2023-06-27 17:34:49 -0700

    Modified analysis scripts (with lots of bugs!)

--- python/analysis/plot_lda_result.py
+++ python/analysis/plot_lda_result.py
@@ -2,6 +2,7 @@ import pickle
 import pathlib
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # import seaborn as sns
 import pandas as pd
@@ -10,12 +11,15 @@ import scipy.io as sio
 import scipy.stats as stats
 
 
-def load_model_id_results(output):
+def load_model_id_results(output, n_pb=4):
+    # load the sinPB results
     acc_python_full = np.stack(
-        [output["sinPB"][i]["vec_acc"][:5] for i in range(len(output["sinPB"]))], axis=1
+        [output["sinPB"][i]["vec_acc"][:n_pb] for i in range(len(output["sinPB"]))],
+        axis=1,
     ).T
     auc_python_full = np.stack(
-        [output["sinPB"][i]["vec_auc"][:5] for i in range(len(output["sinPB"]))], axis=1
+        [output["sinPB"][i]["vec_auc"][:n_pb] for i in range(len(output["sinPB"]))],
+        axis=1,
     ).T
     acc_python_top = [
         output["sinPB"][i]["vec_acc"][0] for i in range(len(output["sinPB"]))
@@ -24,11 +28,14 @@ def load_model_id_results(output):
         output["sinPB"][i]["vec_auc"][0] for i in range(len(output["sinPB"]))
     ]
 
+    # load the sfsPB results
     acc_python_full_sfs = np.stack(
-        [output["sfsPB"][i]["vec_acc"][:5] for i in range(len(output["sfsPB"]))], axis=1
+        [output["sfsPB"][i]["vec_acc"][:n_pb] for i in range(len(output["sfsPB"]))],
+        axis=1,
     ).T
     auc_python_full_sfs = np.stack(
-        [output["sfsPB"][i]["vec_auc"][:5] for i in range(len(output["sfsPB"]))], axis=1
+        [output["sfsPB"][i]["vec_auc"][:n_pb] for i in range(len(output["sfsPB"]))],
+        axis=1,
     ).T
     acc_python_top_sfs = [
         output["sfsPB"][i]["vec_acc"][-1] for i in range(len(output["sfsPB"]))
@@ -49,31 +56,213 @@ def load_model_id_results(output):
     )
 
 
-def plot_lda_results(str_subject="RCS02", str_side="R"):
-    # hard code all paths
-    p_output = pathlib.Path(
-        "/home/jyao/Downloads/biomarker_id/model_id/{}".format(str_subject)
-    )
-    f_output_python = f"{str_subject}_{str_side}_med_avg_auc_LDA.pkl"
-    f_output_matlab = f"{str_subject}_{str_side}_med_level_stats.mat"
+def plot_lda_matlab_results(vec_str_subject, vec_str_side, str_model_id="model_id", n_pb=4, n_rep=5):
+    # quick function for comparing matlab with python results
+
+    vec_auc_python_full = []
+    vec_auc_python_full_sfs = []
+    
+    vec_auc_matlab = []
+    vec_auc_matlab_sfs = []
+    for i in range(len(vec_str_subject)):
+        str_subject = vec_str_subject[i]
+        str_side = vec_str_side[i]
+
+        # load the results
+        p_output = pathlib.Path(
+            f"/home/jyao/Downloads/biomarker_id/{str_model_id}/{str_subject}"
+        )
+        f_output_python = f"{str_subject}_{str_side}_med_avg_auc_LDA.pkl"
+        f_output_matlab = f"{str_subject}_{str_side}_med_level_stats.mat"
+
+        # create output figure folder
+        p_figure_output = pathlib.Path(
+            f"/home/jyao/Downloads/biomarker_id/figures/{str_model_id}/{str_subject}"
+        )
+        p_figure_output.mkdir(parents=True, exist_ok=True)
+
+        # load in the python values
+        output = pickle.load(open(str(p_output / f_output_python), "rb"))
+        (
+            _,
+            auc_python_full,
+            _,
+            _,
+            _,
+            auc_python_full_sfs,
+            _,
+            _,
+        ) = load_model_id_results(output)
+        
+        # append to the outer list
+        vec_auc_python_full.append(auc_python_full)
+        vec_auc_python_full_sfs.append(auc_python_full_sfs)
+        
+        # laod the MATLAB results
+        mat = sio.loadmat(str(p_output / f_output_matlab))
+        auc_matlab = mat["auc_sin"]
+        auc_matlab_sfs = mat["auc_sfs"]
+        
+        vec_auc_matlab.append(auc_matlab)
+        vec_auc_matlab_sfs.append(auc_matlab_sfs)
+        
+        if str_subject == "RCS18":
+            fig = plt.figure()
+            
+            for i in range(n_pb):
+                plt.subplot(1, 4, i + 1)
+                dict_results_auc = {'Program': [], "AUC": []}
+                for j in range(n_rep):
+                    dict_results_auc["Program"].append("Python")
+                    dict_results_auc["AUC"].append(auc_python_full[j, i])
+                
+                dict_results_auc["Program"].append("MATLAB")
+                dict_results_auc["AUC"].append(auc_matlab[i, 0])
+                
+                # plot
+                results_auc = pd.DataFrame(dict_results_auc)
+                sns.boxplot(data=results_auc, x='Program', y='AUC', orient='v')
+                ax = plt.gca()
+
+                # set the labels
+                if i == 0:
+                    ax.set(ylabel='AUC')
+                ax.set(xlabel=None)
+                ax.set(title=f"PB{i + 1}")
+                ax.set(ylim=[0.5, 1])
+
+                # # remove the top and right spines
+                ax.spines['top'].set_visible(False)
+                ax.spines['right'].set_visible(False)
+                
+            fig.savefig(f"{p_figure_output}/auc_comparison.png", dpi=300)
+            plt.close(fig)
+            t1 = 1
+             
+    
+    # # reshape and form pandas dataframe
+    # vec_auc_python_full = np.stack(vec_auc_python_full, axis=0)
+    # vec_auc_python_full_sfs = np.stack(vec_auc_python_full_sfs, axis=0)
+    # vec_auc_matlab = np.stack(vec_auc_matlab, axis=0)
+    # vec_auc_matlab_sfs = np.stack(vec_auc_matlab_sfs, axis=0)
+
+    # # now loop
+    # for i in range(n_pb):
+    # results_dict = {"str_subject": [], "AUC": [], "PB": [], "Method": []}
+    # results_dict_sfs = {"str_subject": [], "AUC": [], "PB": [], "Method": []}
+    # for i in range(len(vec_str_subject)): 
+    #     # load python   
+    #     for j in range(n_pb):
+    #         for k in range(n_rep):
+    #             # load sin PB
+    #             results_dict["str_subject"].append(vec_str_subject[i])
+    #             results_dict["PB"].append(j + 1)
+    #             results_dict["AUC"].append(vec_auc_python_full[i, k, j])
+    #             results_dict["Method"].append("Python")
+                
+    #             # load sfs PB
+    #             results_dict_sfs["str_subject"].append(vec_str_subject[i])
+    #             results_dict_sfs["PB"].append(j + 1)
+    #             results_dict_sfs["AUC"].append(vec_auc_python_full_sfs[i, k, j])
+    #             results_dict_sfs["Method"].append("Python")
+                
+    #     # load MATLAB
+    #     for j in range(n_pb):
+    # t1 = 1
+        
+        
+        
+        # # plot the figure for auc
+        # fig = plt.figure()
+        # sns.boxplot(data=results_auc, x='Program', y='AUC', orient='v')
+        # ax = plt.gca()
+
+        # # set the labels
+        # ax.set(xlabel=None)
+        # ax.set(ylabel='AUC with LDA')
+        # ax.set(title='AUC with LDA for Top 1 Power Band')
+
+        # # set the ranges
+        # ax.set_ylim([0.62, 0.80])
+
+        # # remove the top and right spines
+        # ax.spines['top'].set_visible(False)
+        # ax.spines['right'].set_visible(False)
+
+        # fig.savefig(str(p_figure_output / 'RCS02_R_med_level_auc_LDA.png'), dpi=300)
+        # plt.close(fig)
+
+        # # plot the figure for accuracy
+        # fig = plt.figure()
+        # sns.boxplot(data=results_auc, x='Program', y='ACC', orient='v')
+        # ax = plt.gca()
+
+        # # set the labels
+        # ax.set(xlabel=None)
+        # ax.set(ylabel='ACC with LDA')
+        # ax.set(title='ACC with LDA for Top 1 Power Band')
+
+        # # set the ranges
+        # ax.set_ylim([0.60, 0.75])
+
+        # # remove the top and right spines
+        # ax.spines['top'].set_visible(False)
+        # ax.spines['right'].set_visible(False)
+
+
+def plot_lda_results(str_model_id="model_id", str_subject="RCS02", str_side="R"):
+    
+    if str_model_id == "model_id_woart":
+        # hard code all paths
+        p_output = pathlib.Path(
+            "/home/jyao/Downloads/biomarker_id/dynamics_woart/{}".format(str_subject)
+        )
+        f_output_python = f"{str_subject}_{str_side}_med_avg_auc_LDA_dynamics_woart.pkl"
+        
+        output_full = pickle.load(open(str(p_output / f_output_python), "rb"))
+        
+        output = dict()
+        output['sinPB'] = output_full['sinPB']["n_dynamics_1"]
+        output['sfsPB'] = output_full['sfsPB']["n_dynamics_1"]
+        (
+            acc_python_full,
+            auc_python_full,
+            acc_python_top,
+            auc_python_top,
+            acc_python_full_sfs,
+            auc_python_full_sfs,
+            acc_python_top_sfs,
+            auc_python_top_sfs,
+        ) = load_model_id_results(output)
+        
+    else:
+        # hard code all paths
+        p_output = pathlib.Path(
+            "/home/jyao/Downloads/biomarker_id/{}/{}".format(str_model_id, str_subject)
+        )
+        f_output_python = f"{str_subject}_{str_side}_med_avg_auc_LDA.pkl"
+        
+        output = pickle.load(open(str(p_output / f_output_python), "rb"))
+        (
+            acc_python_full,
+            auc_python_full,
+            acc_python_top,
+            auc_python_top,
+            acc_python_full_sfs,
+            auc_python_full_sfs,
+            acc_python_top_sfs,
+            auc_python_top_sfs,
+        ) = load_model_id_results(output)
+        
+        
 
     p_figure_output = pathlib.Path(
-        "/home/jyao/Downloads/biomarker_id/figures/model_id/{}".format(str_subject)
+        "/home/jyao/Downloads/biomarker_id/figures/{}/{}".format(str_model_id, str_subject)
     )
     p_figure_output.mkdir(parents=True, exist_ok=True)
 
     # load in the python values
-    output = pickle.load(open(str(p_output / f_output_python), "rb"))
-    (
-        acc_python_full,
-        auc_python_full,
-        acc_python_top,
-        auc_python_top,
-        acc_python_full_sfs,
-        auc_python_full_sfs,
-        acc_python_top_sfs,
-        auc_python_top_sfs,
-    ) = load_model_id_results(output)
+
 
     # # plot the figure for auc
     # fig = plt.figure()
@@ -234,7 +423,7 @@ def plot_lda_results(str_subject="RCS02", str_side="R"):
     ) = load_model_id_results(output_RNN)
 
     fig = plt.figure()
-    idx = np.arange(1, 6, 1)
+    idx = np.arange(1, 5, 1)
     x_ticks = [str(x) for x in idx]
     plt.plot(idx, np.mean(auc_python_full_sfs, axis=0), "r", label="LDA")
     plt.fill_between(
@@ -302,7 +491,7 @@ def plot_lda_results(str_subject="RCS02", str_side="R"):
 
     plt.xlabel("Number of Power Bands Included")
     plt.ylabel("AUC with Algorithms")
-    plt.title(f"Comparison of AUC for Top 5 Power Bands: {str_subject}_{str_side}")
+    plt.title(f"Comparison of AUC for Top 4 Power Bands: {str_subject}_{str_side}")
     plt.legend(frameon=False)
     plt.xticks(idx, x_ticks)
 
@@ -324,6 +513,14 @@ def plot_lda_results(str_subject="RCS02", str_side="R"):
 
 
 if __name__ == "__main__":
+    # plot_lda_matlab_results(["RCS02", "RCS11", "RCS12", "RCS18"], ["R", "L", "L", "L"])
     # plot_lda_results(str_subject="RCS02", str_side="R")
     plot_lda_results(str_subject="RCS08", str_side="R")
+    # plot_lda_results(str_subject="RCS11", str_side="L")
+    # plot_lda_results(str_subject="RCS12", str_side="L")
+    # plot_lda_results(str_subject="RCS18", str_side="L")
     # plot_lda_results(str_subject="RCS17", str_side="L")
+    
+    
+    # plot_lda_results(str_subject="RCS02", str_side="R", str_model_id='model_id_woart')
+#
--- python/biomarker/run_scripts_id/run_biomarker_id_sweep_ur.sh
+++ python/biomarker/run_scripts_id/run_biomarker_id_sweep_ur.sh
@@ -6,10 +6,6 @@ conda activate envTorch201
 # HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=home_ray_cpu_batch patient=RCS08R preproc=ur6s
 # HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=home_ray_cpu_batch patient=RCS11L preproc=ur6s
 
-# HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --config-name config_sweep_ur_model parallel=home_ray_cpu_batch patient=RCS08R preproc=ur6s meta=med_RF_auc_default
-HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --config-name config_sweep_ur_model parallel=home_ray_cpu_batch patient=RCS08R preproc=ur6s meta=med_RNN_auc_default
-HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --config-name config_sweep_ur_model parallel=home_ray_cpu_batch patient=RCS08R preproc=ur6s meta=med_SVM_auc_default
-
 # UR of 7_5s
 HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=home_ray_cpu_batch patient=RCS02R preproc=ur7_5s
 HYDRA_FULL_ERROR=1 python ../training/biomarker_id.py --multirun --config-name config_sweep_ur_model parallel=home_ray_cpu_batch patient=RCS08R preproc=ur7_5s
